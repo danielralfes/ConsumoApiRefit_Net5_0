dc6dab594b866f010951c2fe7a2de18abb2882d0
Daniel Ralfes Silva de Abreu <danielralfes@gmail.com>
2020-12-21 09:36:47 -0300

    Criação do Projeto

---
+++ ICepApiService.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoApiRefit_Net5_0
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json/")]
+        Task<CepResponse> GetAdressAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsumoApiRefit_Net5_0
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await ConsumoDireto();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro na consulta ao cep: " + ex.Message);
+            }
+
+            static async Task ConsumoDireto()
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu cep:");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Cep informado:" + cepInformado);
+
+                var address = await cepClient.GetAdressAsync(cepInformado);
+
+                Console.WriteLine($"\n {address.Logradouro} \n {address.Bairro} \n {address.Localidade}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
